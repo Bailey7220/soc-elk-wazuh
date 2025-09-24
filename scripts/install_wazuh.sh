6dc0f1ff8cf36f5d83feb3e43a0cfae47af6f138
Bailey Collins <103609609+Bailey7220@users.noreply.github.com>
2025-09-24 02:07:25 -0500

    Update install_wazuh.sh

--- scripts/install_wazuh.sh
+++ scripts/install_wazuh.sh
@@ -3,11 +3,11 @@ set -e
 
 # 1. Add Wazuh repository
 curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
-echo "deb https://packages.wazuh.com/4.x/apt stable main" \
-  | tee /etc/apt/sources.list.d/wazuh.list
+echo "deb https://packages.wazuh.com/4.x/apt stable main" | \
+  tee /etc/apt/sources.list.d/wazuh.list
 apt-get update
 
-# 2. Install Wazuh Manager & API
+# 2. Install Wazuh Manager
 apt-get install -y wazuh-manager
 
 # 3. Install Filebeat

b585169c85adc4ec353363709df4a0bd5e33858c
Bailey Collins <103609609+Bailey7220@users.noreply.github.com>
2025-09-24 02:07:06 -0500

    Update install_elk.sh

--- scripts/install_elk.sh
+++ scripts/install_elk.sh
@@ -3,8 +3,8 @@ set -e
 
 # 1. Add Elastic APT repo
 curl -s https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" \
-  | tee /etc/apt/sources.list.d/elastic-7.x.list
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | \
+  tee /etc/apt/sources.list.d/elastic-7.x.list
 apt-get update
 
 # 2. Install Elasticsearch

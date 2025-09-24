13c614d6ea5d00c7fa3b752ff2d8b6c6abe5a8dd
Bailey Collins <103609609+Bailey7220@users.noreply.github.com>
2025-09-23 23:55:11 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,10 @@
 variable "my_ip" {
-  description = "Your IP in CIDR (e.g., 1.2.3.4/32)"
+  description = "Your IP in CIDR (68.229.244.149/32)"
   type        = string
+}
+
+variable "public_key_path" {
+  description = "Absolute path to your SSH public key"
+  type        = string
+  default     = "/c/Users/OKCMV/.ssh/id_ed25519.pub"
 }

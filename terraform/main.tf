680ca987432738e252500987250b33fd3594032c
Bailey Collins <103609609+Bailey7220@users.noreply.github.com>
2025-09-24 00:25:47 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,23 +43,23 @@ resource "aws_security_group" "soc_sg" {
   }
 }
 
-resource "aws_instance" "soc_vm" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.soc_key.key_name
-  vpc_security_group_ids = [aws_security_group.soc_sg.id]
-
-  tags = {
-    Name = "soc-elk-wazuh"
-  }
-}
-
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "soc_vm" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"          # Changed to a Free Tier–eligible type
+  key_name               = aws_key_pair.soc_key.key_name
+  vpc_security_group_ids = [aws_security_group.soc_sg.id]
+
+  tags = {
+    Name = "soc-elk-wazuh"
   }
 }
 
